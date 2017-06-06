f6bb63d7783501aa97eb06b68d528dfe52638d0b
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-06-05 20:12:05 -0500

    navbar now shows and hides appropriately

--- app/controllers/controller.home.js
+++ app/controllers/controller.home.js
@@ -3,11 +3,13 @@
 module.exports = function($scope, $window, DataFactory) {
 
 	$scope.getPsalmsData = () => {
+        $("#navButtons").show();
 		DataFactory.getPsalmsJSON();
 		$window.location.href="#!/ControlData";
 	};
 
 	$scope.getTestData = () => {
+        $("#navButtons").show();
 		DataFactory.getTestJSON();
 		$window.location.href="#!/ControlData";
 	};
